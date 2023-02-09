d6f6c0bcb1abf4ebf9d0f2c24616144b9e060c87
이 다정 <dew0489@naver.com>
2023-02-09 11:47:29 +0900

    s3 imageRouter 추가

--- server/src/router/categoryRouter.js
+++ server/src/router/categoryRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { categoryController } = require("../controller");
 const { categoryMiddleware } = require("../middleware");
+const { userMiddleware } = require("../middleware");
+const { imageMiddleware } = require("../middleware/imageMiddleware");
 
 const categoryRouter = express.Router();
 
 //카테고리 생성
 categoryRouter.post(
   "/",
+  userMiddleware.verifyUser,
+  userMiddleware.verifyAdmin,
   categoryMiddleware.checkCompleteCategoryFrom("body"),
   categoryController.createCategory
 );
@@ -25,6 +29,8 @@ categoryRouter.get(
 //카테고리 수정
 categoryRouter.put(
   "/:id",
+  userMiddleware.verifyUser,
+  userMiddleware.verifyAdmin,
   categoryMiddleware.checkCategoryIdFrom("params"),
   categoryMiddleware.checkMinCategoryConditionFrom("body"),
   categoryController.putCategory
@@ -32,9 +38,19 @@ categoryRouter.put(
 //카테고리 삭제
 categoryRouter.delete(
   "/:id",
+  userMiddleware.verifyUser,
+  userMiddleware.verifyAdmin,
   categoryMiddleware.checkCategoryIdFrom("params"),
   categoryMiddleware.checkRemainedProduct("params"),
   categoryController.deleteCategory
 );
+
+// categoryRouter.post(
+//   '/test/image',
+//   imageMiddleware.imageUploader.array('image'),
+//   (req, res) => {
+//     res.send('good!')
+//   }
+// )
 
 module.exports = categoryRouter;
