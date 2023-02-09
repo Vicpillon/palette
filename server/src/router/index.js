a56282b14bdd9ba8ef4854877c8083950063d611
[AI6/2반] 곽희웅 <gmldnd666@gmail.com>
2023-02-09 14:55:28 +0900

    라우터 수정

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -4,8 +4,10 @@ const categoryRouter = require("./categoryRouter");
 const orderRouter = require("./orderRouter");
 const adminRouter = require("./adminRouter");
 const userRouter = require("./userRouter");
-const imageRouter = require('./imageRouter');
+const imageRouter = require("./imageRouter");
+const authRouter = require("./authRouter");
 const v1Router = express.Router();
+const cors = require("cors");
 
 v1Router.use(cors());
 v1Router.use("/products", productRouter);
@@ -14,6 +16,7 @@ v1Router.use("/orders", orderRouter);
 v1Router.use("/admin", adminRouter);
 v1Router.use("/users", userRouter);
 v1Router.use("/images", imageRouter);
+v1Router.use("/auth", authRouter);
 
 module.exports = {
   v1: v1Router,
