160db0351c421d0eb5c805b0d4e0be3e718ad4fb
[AI6/2반] 곽희웅 <gmldnd666@gmail.com>
2023-02-09 21:10:20 +0900

    isAdmin 추가

--- server/src/misc/util.js
+++ server/src/misc/util.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-// const jwt_decode = require("jwt-decode");
+const jwt_decode = require("jwt-decode");
 
 function sanitizeObject(obj) {
   const result = Object.entries(obj).reduce((map, [key, value]) => {
@@ -23,11 +23,12 @@ const setUserToken = (res, user) => {
   // 토큰을 쿠키로 전달
   const token = jwt.sign(user, process.env.SECRET, { expiresIn: 600 });
   // expiresIn : 토큰에 대한 기한 "1d" "1h" 60*60(s) 60:1minute
-  // const decode_token = jwt_decode(token)
-
+  const decode_token = jwt_decode(token);
+  const isAdmin = decode_token.role;
+  const data = { token: token, isAdmin: isAdmin };
   res
     .cookie("token", token, { maxAge: 2628002880, httpOnly: true })
-    .json(buildResponse(token));
+    .json(buildResponse(data));
   // `${decode_token.role}`
   /* res.cookie('token', token, { httpOnly: false, sameSite:"None", maxAge: 30000(ms), secure: false }).json(token)
   maxAge : 쿠키에 대한 기한  
